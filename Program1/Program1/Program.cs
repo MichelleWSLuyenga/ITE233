d1f42f1438d9651c8c1582b35ae1ad03facf0d4c
Michelle Luyenga <wn1apc@gmail.com>
2017-10-16 00:58:09 +0700

    Learning

---
+++ Program1/Program1/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Program1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int TotalWeek = 0;
+            while(TotalWeek != 13)
+            {
+                Console.Write("How many week does stamford has for each semester");
+                TotalWeek = Convert.ToInt16(Console.ReadLine());
+                if(TotalWeek != 13)
+                {
+                    Console.WriteLine("Are you sure that you are correct?");
+                }
+            }
+            Console.WriteLine("Stamford Week Countdown");
+            for(int i = 1; i <= TotalWeek; i++)
+            {
+                switch(i)
+                {
+                    case 6:
+                        Console.WriteLine("Time For Midterm Exam!!");
+                        break;
+                    case 13:
+                        Console.WriteLine("Time for Final Exam, I love it!");
+                        break;
+                    default:
+                        Console.WriteLine("Week" + i + " Lecture");
+                        break;
+                }
+            }
+            Console.WriteLine("Time for term break! See you again next Semester.");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Sleepy/Sleepy/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sleepy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("how many day's temperature?");
+            int days = Convert.ToInt16(Console.ReadLine());
+            int[] eachdaystemp = new int[days];
+            double avg = 0;
+            //{day, temp}
+            int[] hottest = { 0, 0 };
+            int[] coldest = { 0, 0 };
+            //Console.WriteLine(eachdaystemp.Length);
+            //Console.ReadKey();
+            for(int i = 0; i < eachdaystemp.Length; i++)
+            {
+                Console.WriteLine("Day" + i+1 + "'s temp:");
+                eachdaystemp[i] = Convert.ToInt16(Console.ReadLine());
+                avg += eachdaystemp[i];
+
+                if (i == 0)
+                {
+                    hottest[0] = i + 1;
+                    hottest[1] = eachdaystemp[i];
+                    coldest[0] = i + 1;
+                    coldest[1] = eachdaystemp[i];
+                }
+                if(hottest[1] < eachdaystemp[i])
+                {
+                    hottest[0] = i + 1;
+                    hottest[1] = eachdaystemp[i];
+                }
+                if(coldest[1] > eachdaystemp[i])
+                {
+                    coldest[0] = i + 1;
+                    coldest[1] = eachdaystemp[i];
+                }
+
+
+            }
+            avg /= days;
+
+
+            Console.ReadKey();
+        }
+    }
+}
