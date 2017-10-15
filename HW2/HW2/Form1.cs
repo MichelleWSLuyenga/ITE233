72d8c20afee5709234e78670aa7e628e2d6ad426
Michelle Luyenga <wn1apc@gmail.com>
2017-10-16 00:33:13 +0700

    Add files via upload

---
+++ HW2/HW2/Form1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HW2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            String te1;
+           te1 = Convert.ToString(textBox1.Text);
+            textBox3.Text = Convert.ToString(te1);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            String te2;
+            te2 = Convert.ToString(textBox2.Text);
+            textBox3.Text = Convert.ToString(te2);
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+           // textBox1.Text = Text;
+        }
+    }
+}
---
+++ HomeWork1/HomeWork1/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWork1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = 0;
+
+            do
+            {
+                Console.Write("Enter level of pyramid (enter -1 to exit): ");
+                num = Convert.ToInt16(Console.ReadLine());
+
+                if(num != -1)
+                {
+                    for(int i = 1; i <= num; i++)
+                    {
+                        for(int j = 1; j <= i; j++)
+                        {
+                            Console.Write(" ");
+                        }
+                        for(int n = num; n >= i; n--)
+                        {
+                            Console.Write("* ");
+                        }
+                        Console.WriteLine();
+                    }
+                }else
+                {
+                    Console.WriteLine("Thank you!");
+                }
+
+            } while (num != -1);
+
+            Console.ReadKey();
+        }
+    }
+}
